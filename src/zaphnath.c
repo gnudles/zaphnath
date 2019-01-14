66ee1773a11d04e0b604f3fb579dd04fce9922cf
Orr Shalom Dvory <dvoreader@gmail.com>
2019-01-14 16:52:03 +0200

    made a better shuffle.

--- src/zaphnath.c
+++ src/zaphnath.c
@@ -11,24 +11,36 @@ if(B < 64){temp=Y[0];Y[0]=(Y[0]<<(B))|(Y[1]>>(64-B));Y[1]=(Y[1]<<(B))|(temp>>(64
 else{temp=Y[0];Y[0]=(Y[1]<<(B-64))|(Y[0]>>(128-B));Y[1]=(temp<<(B-64))|(Y[1]>>(128-B));}\
 }
 #define ZPN_DIFFUSE_ROUND(A, B, C, R, T)\
-A+=B; C^=A; C=ROTL64(C,R)^T;C-=ROTL64(B,15)
+C^=A; C=ROTL64(C,R)+T; A+=B; C-=ROTL64(B,16)+ROTL64(B,46)+ROTL64(B,50)+ROTL64(B,52)
 
 #define REV_ZPN_DIFFUSE_ROUND(A, B, C, R, T)\
-C+=ROTL64(B,15);C=ROTL64((C^T),64-R);C^=A; A-=B
+C+=ROTL64(B,16)+ROTL64(B,46)+ROTL64(B,50)+ROTL64(B,52); A-=B; C=ROTL64((C-T),64-R); C^=A
 
 #define ZPN_DIFFUSE256(A,B,C,D,I,J,K,L) \
-ZPN_DIFFUSE_ROUND(D,A,B,25,I);\
+ZPN_DIFFUSE_ROUND(D,A,B,20,I);\
 ZPN_DIFFUSE_ROUND(C,B,A,11,J);\
-ZPN_DIFFUSE_ROUND(C,D,B,51,K);\
-ZPN_DIFFUSE_ROUND(A,B,D,48,L);
+ZPN_DIFFUSE_ROUND(B,D,C,30,K);\
+ZPN_DIFFUSE_ROUND(A,B,D,51,L);
 
 #define REV_ZPN_DIFFUSE256(A,B,C,D,I,J,K,L) \
-REV_ZPN_DIFFUSE_ROUND(A,B,D,48,L);\
-REV_ZPN_DIFFUSE_ROUND(C,D,B,51,K);\
+REV_ZPN_DIFFUSE_ROUND(A,B,D,51,L);\
+REV_ZPN_DIFFUSE_ROUND(B,D,C,30,K);\
 REV_ZPN_DIFFUSE_ROUND(C,B,A,11,J);\
-REV_ZPN_DIFFUSE_ROUND(D,A,B,25,I);
+REV_ZPN_DIFFUSE_ROUND(D,A,B,20,I);
+
+//This is for those who concern about multiplication
+#define MUL18150808750145(V,TEMP)\
+TEMP=(V)+((V)<<5);\
+TEMP=(TEMP<<6)+(TEMP<<23)+(TEMP<<39);\
+V=V+TEMP+(V<<17)+(V<<33)
+#define MUL4065217(V,TEMP)\
+TEMP=(V<<1)+(V<<2);\
+TEMP+=(TEMP<<2)+V;\
+TEMP+=(TEMP<<11);\
+V=(TEMP<<6)+V
 
 /* there are exactly 128 primitive roots of 257 */
+
 uint8_t prim_root[128]={3 , 5 , 6 , 7 , 10 , 12 , 14 , 19 , 20 , 24 , 27 , 28 , 33 , 37 , 38 , 39 , 40 , 41 , 43 , 45 , 47 , 48 , 51 , 53 , 54 , 55 , 56 , 63 , 65 , 66 , 69 , 71 , 74 , 75 , 76 , 77 , 78 , 80 , 82 , 83 , 85 , 86 , 87 , 90 , 91 , 93 , 94 , 96 , 97 , 101 , 102 , 103 , 105 , 106 , 107 , 108 , 109 , 110 , 112 , 115 , 119 , 125 , 126 , 127 , 130 , 131 , 132 , 138 , 142 , 145 , 147 , 148 , 149 , 150 , 151 , 152 , 154 , 155 , 156 , 160 , 161 , 163 , 164 , 166 , 167 , 170 , 171 , 172 , 174 , 175 , 177 , 179 , 180 , 181 , 182 , 183 , 186 , 188 , 191 , 192 , 194 , 201 , 202 , 203 , 204 , 206 , 209 , 210 , 212 , 214 , 216 , 217 , 218 , 219 , 220 , 224 , 229 , 230 , 233 , 237 , 238 , 243 , 245 , 247 , 250 , 251 , 252 , 254 };
 // you get the inverse by pow(p,255)%257
 uint8_t inv_prim_root[128]={86, 103, 43, 147, 180, 150, 202, 230, 90, 75, 238, 101, 148, 132, 115, 145, 45, 163, 6, 40, 175, 166, 126, 97, 119, 243, 179, 102, 87, 74, 149, 181, 66, 24, 186, 247, 201, 151, 210, 96, 127, 3, 65, 20, 209, 152, 216, 83, 53, 28, 63, 5, 164, 177, 245, 188, 224, 250, 218, 38, 54, 220, 51, 85, 172, 206, 37, 203, 219, 39, 7, 33, 69, 12, 80, 93, 252, 194, 229, 204, 174, 41, 105, 48, 237, 192, 254, 130, 161, 47, 106, 56, 10, 71, 233, 191, 76, 108, 183, 170, 155, 78, 14, 138, 160, 131, 91, 82, 217, 251, 94, 212, 112, 142, 125, 109, 156, 19, 182, 167, 27, 55, 107, 77, 110, 214, 154, 171};
@@ -74,8 +86,8 @@ void zpn_imat(uint64_t *in, uint64_t *out)
 }
 void zpn_mixbit(uint64_t* x,uint64_t *out)
 {
-	uint64_t b1=0x4c6ba2e9fff0000f;
-	uint64_t b2=0x53a63a2bf0f0ff00;
+	uint64_t b1=0x4c6ba2e966660f0f;
+	uint64_t b2=0x53a63a2b3333ff00;
 	uint64_t m[4]={b1&b2,b1&(~b2),b2&(~b1),~b1&~b2};
 	out[0]=(m[0]&x[0])|(m[1]&x[1])|(m[2]&x[2])|(m[3]&x[3]);
 	out[1]=(m[0]&x[1])|(m[1]&x[0])|(m[2]&x[3])|(m[3]&x[2]);
@@ -154,6 +166,8 @@ int zpn_expand_key(uint8_t *key, uint32_t length, uint32_t cycles, struct zpn_ke
 		{
 			zpn_feed_sponge(sponge,sponge_len,0);
 			ts->cxor[i/4][i%4]=((uint64_t*)sponge)[0];
+			zpn_feed_sponge(sponge,sponge_len,0);
+			ts->cadd[i/4][i%4]=((uint64_t*)sponge)[0];
 		}
 		free(sponge);
 	}
@@ -178,12 +192,13 @@ int zpn_expand_key(uint8_t *key, uint32_t length, uint32_t cycles, struct zpn_ke
 		for (i=0; i < cycles*4; ++i)
 		{
 			ts->cxor[i/4][i%4]=i;
+			ts->cadd[i/4][i%4]=i<<5;
 		}
 	}
 	return 0;
 }
 
-void zpn_encrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *data, uint8_t *out)
+void zpn_encrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *raw, uint8_t *enc)
 {
 	uint64_t counter_df[4];
 	counter_df[0]=counter;
@@ -194,60 +209,115 @@ void zpn_encrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t
 	ZPN_DIFFUSE256(counter_df[0], counter_df[1], counter_df[2], counter_df[3],
 	0xa5a5a5a578787878,0x2d2d2d2d1e1e1e1e,0x969696964b4b4b4b,0x3c3c3c3cf0f0f0f0);
 	uint32_t i, b;
-#define o64 ((uint64_t*)out)
-	o64[0]=((uint64_t*)data)[0]^counter_df[0];
-	o64[1]=((uint64_t*)data)[1]^counter_df[1];
-	o64[2]=((uint64_t*)data)[2]^counter_df[2];
-	o64[3]=((uint64_t*)data)[3]^counter_df[3];
+#define o64 ((uint64_t*)enc)
+	o64[0]=((uint64_t*)raw)[0]^counter_df[0];
+	o64[1]=((uint64_t*)raw)[1]^counter_df[1];
+	o64[2]=((uint64_t*)raw)[2]^counter_df[2];
+	o64[3]=((uint64_t*)raw)[3]^counter_df[3];
 	uint64_t temp[4];
+/*	o64[0]*=18150808750145UL;//inverse: *4065217
+	o64[1]*=18150808750145UL;//inverse: *4065217
+	o64[2]*=18150808750145UL;//inverse: *4065217
+	o64[3]*=18150808750145UL;//inverse: *4065217
+*/
+	MUL18150808750145(o64[0],temp[0]);
+	MUL18150808750145(o64[1],temp[0]);
+	MUL18150808750145(o64[2],temp[0]);
+	MUL18150808750145(o64[3],temp[0]);
 	for (i =0 ; i< key->cycles ; ++i)
 	{
 		ZPN_DIFFUSE256(o64[0], o64[1], o64[2], o64[3],key->cxor[i][0],
 		key->cxor[i][1],key->cxor[i][2],key->cxor[i][3]);
-		zpn_mat(o64,temp);
-#define ROTF 13
+		zpn_mixbit(o64,temp);
+		temp[0]=ROTL64(temp[0],61);
+		temp[1]=ROTL64(temp[1],30);
+		temp[2]=ROTL64(temp[2],41);
+		temp[3]=ROTL64(temp[3],19);
+		zpn_imat(temp,o64);
+#define ROTF 55
 		
-		o64[0]=(temp[0]<<ROTF)|(temp[3]>>(64-ROTF));
-		o64[1]=(temp[1]<<ROTF)|(temp[0]>>(64-ROTF));
-		o64[2]=(temp[2]<<ROTF)|(temp[1]>>(64-ROTF));
-		o64[3]=(temp[3]<<ROTF)|(temp[2]>>(64-ROTF));
-		zpn_mat(o64,temp);
-		zpn_mixbit(temp,o64);
+		temp[0]=((o64[0]<<ROTF)|(o64[3]>>(64-ROTF)))+key->cadd[i][0];
+		temp[1]=((o64[1]<<ROTF)|(o64[0]>>(64-ROTF)))+key->cadd[i][1];
+		temp[2]=((o64[2]<<ROTF)|(o64[1]>>(64-ROTF)))+key->cadd[i][2];
+		temp[3]=((o64[3]<<ROTF)|(o64[2]>>(64-ROTF)))+key->cadd[i][3];
+#undef ROTF 
+		zpn_mat(temp,o64);
 	}
+
+/*
+	o64[0]*=4065217;
+	o64[1]*=4065217;
+	o64[2]*=4065217;
+	o64[3]*=4065217;
+*/
+	MUL4065217(o64[0],temp[0]);
+	MUL4065217(o64[1],temp[0]);
+	MUL4065217(o64[2],temp[0]);
+	MUL4065217(o64[3],temp[0]);
+
 	o64[0]^=key->final_xor[0];
 	o64[1]^=key->final_xor[1];
 	o64[2]^=key->final_xor[2];
 	o64[3]^=key->final_xor[3];
 }
 
-void zpn_decrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *data, uint8_t *out)
+void zpn_decrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *raw, uint8_t *enc)
 {
-#define d64 ((uint64_t*)data)
+#define d64 ((uint64_t*)raw)
 	d64[0]=o64[0]^key->final_xor[0];
 	d64[1]=o64[1]^key->final_xor[1];
 	d64[2]=o64[2]^key->final_xor[2];
 	d64[3]=o64[3]^key->final_xor[3];
 #undef o64
-	
-
-	uint32_t i, b;
-
 	uint64_t temp[4];
-	for (i = key->cycles  ; i>=1 ; --i)
+/*	
+	d64[0]*=18150808750145UL;//inverse: *4065217
+	d64[1]*=18150808750145UL;//inverse: *4065217
+	d64[2]*=18150808750145UL;//inverse: *4065217
+	d64[3]*=18150808750145UL;//inverse: *4065217
+*/
+	MUL18150808750145(d64[0],temp[0]);
+	MUL18150808750145(d64[1],temp[0]);
+	MUL18150808750145(d64[2],temp[0]);
+	MUL18150808750145(d64[3],temp[0]);
+	int32_t i, b;
+
+#define t64 temp
+	for (i = key->cycles - 1 ; i >= 0 ; --i)
 	{
-		zpn_mixbit(d64,temp);
-		zpn_imat(temp,d64);
-#define ROTF 13
-		temp[0]=(d64[0]>>ROTF)|(d64[1]<<(64-ROTF));
-		temp[1]=(d64[1]>>ROTF)|(d64[2]<<(64-ROTF));
-		temp[2]=(d64[2]>>ROTF)|(d64[3]<<(64-ROTF));
-		temp[3]=(d64[3]>>ROTF)|(d64[0]<<(64-ROTF));
+		zpn_imat(d64,temp);
+		t64[0]-=key->cadd[i][0];
+		t64[1]-=key->cadd[i][1];
+		t64[2]-=key->cadd[i][2];
+		t64[3]-=key->cadd[i][3];
+#define ROTF 55
+		d64[0]=(t64[0]>>ROTF)|(t64[1]<<(64-ROTF));
+		d64[1]=(t64[1]>>ROTF)|(t64[2]<<(64-ROTF));
+		d64[2]=(t64[2]>>ROTF)|(t64[3]<<(64-ROTF));
+		d64[3]=(t64[3]>>ROTF)|(t64[0]<<(64-ROTF));
+#undef ROTF
+		zpn_mat(d64,t64);
+		temp[0]=ROTL64(temp[0],3);
+		temp[1]=ROTL64(temp[1],34);
+		temp[2]=ROTL64(temp[2],23);
+		temp[3]=ROTL64(temp[3],45);
 
-		zpn_imat(temp,d64);
 
-		REV_ZPN_DIFFUSE256(d64[0], d64[1], d64[2], d64[3],key->cxor[i-1][0],
-		key->cxor[i-1][1],key->cxor[i-1][2],key->cxor[i-1][3]);
+		zpn_mixbit(temp,d64);
+		REV_ZPN_DIFFUSE256(d64[0], d64[1], d64[2], d64[3],key->cxor[i][0],
+		key->cxor[i][1],key->cxor[i][2],key->cxor[i][3]);
+
 	}
+	MUL4065217(d64[0],temp[0]);
+	MUL4065217(d64[1],temp[0]);
+	MUL4065217(d64[2],temp[0]);
+	MUL4065217(d64[3],temp[0]);
+/*
+	d64[0]*=4065217UL;
+	d64[1]*=4065217UL;
+	d64[2]*=4065217UL;
+	d64[3]*=4065217UL;
+*/
 	uint64_t counter_df[4];
 	counter_df[0]=counter;
 	counter_df[1]=key->counter_mask[0];
--- src/zaphnath.h
+++ src/zaphnath.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#define MAX_CYCLES 50
+#define MAX_CYCLES 20
 
 struct zpn_key
 {
@@ -9,9 +9,10 @@ struct zpn_key
 	uint64_t final_xor[4];
 	uint8_t lookup[2][256];
 	uint64_t cxor[MAX_CYCLES][4];
+	uint64_t cadd[MAX_CYCLES][4];
 };
 int zpn_expand_key(uint8_t *key, uint32_t length, uint32_t cycles, struct zpn_key * ts);
 
-void zpn_encrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *data, uint8_t *out);
-void zpn_decrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *data, uint8_t *out);
+void zpn_encrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *raw, uint8_t *enc);
+void zpn_decrypt(uint64_t nounce, uint64_t counter, struct zpn_key *key, uint8_t *raw, uint8_t *enc);
 
